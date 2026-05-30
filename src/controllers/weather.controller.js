b96152efafffcc25220f4c404bdfc9bd4b5c68c4
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2026-05-29 22:53:27 -0400

    Refactor weather response format in getWeatherByCityController to include detailed weather data

--- src/controllers/weather.controller.js
+++ src/controllers/weather.controller.js
@@ -5,7 +5,12 @@ export const getWeatherByCityController = async (req, res) => {
     try {
         const data = await getWeatherByCity(city)
 
-        res.json(data)
+        res.json({
+            city: data.address,
+            temperature: data.days[0].temp,
+            humidity: data.days[0].humidity,
+            conditions: data.description
+        })
     } catch (error) {
         res.status(error.status || 500).json({
             message: "Failed to fetch weather data",
